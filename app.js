3fc46368f4eef243504f480a683dfb3137c0e481
jiwoo <dlwldn1346@gmail.com>
2020-07-13 16:06:50 +0900

    second

--- app.js
+++ app.js
@@ -1,15 +1,26 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5000;
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
 
 app.set('views', `${__dirname}/public`);
 app.set('view engine', 'html');
 app.engine('html', require('ejs').renderFile)
-app.use(express.static('src', {
+app.use(express.static('public', {
     extensions: ['html']
 }))
 
+io.on('connection', (socket) => {
+    console.log("User connected...");
+    socket.on('chatting', (data) => {
+        io.sockets.emit('chatting', data);
+    });
+})
+
+
 app.get('/', (req, res) => {
+    console.log(__dirname);
     res.render('index');
 });
 
@@ -22,6 +33,10 @@ app.get('/api/users', (req, res) => {
     res.send(user);
 })
 
-app.listen(PORT, () => {
-    console.log(`server is running on ${PORT}`);
-});
+// app.listen(PORT, () => {
+//     console.log(`server is running on ${PORT}`);
+// });
+
+http.listen(PORT, () => {
+    console.log(`listening on localhost:${PORT}`);
+})
